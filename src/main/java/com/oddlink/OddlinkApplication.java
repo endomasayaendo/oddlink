312892388f44bea789e8d45a3d293df942ef356d
endomasayaendo <en0720do@gmail.com>
2026-01-14 21:34:14 +0900

    first commit

---
+++ src/main/java/com/oddlink/OddlinkApplication.java
@@ -0,0 +1,13 @@
+package com.oddlink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OddlinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OddlinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oddlink/OddlinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oddlink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OddlinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
